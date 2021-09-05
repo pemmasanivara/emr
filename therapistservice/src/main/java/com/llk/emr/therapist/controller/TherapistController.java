a937bb5aa78da13c2ed3af90d4d5c00683d71ad9
Varaprasad.pemmasan <Varaprasad.pemmasan@ihsmarkit.com>
2021-09-05 23:07:10 +0530

    Changing the commit message if that is not correct

--- therapistservice/src/main/java/com/llk/emr/therapist/controller/TherapistController.java
+++ therapistservice/src/main/java/com/llk/emr/therapist/controller/TherapistController.java
@@ -1,9 +1,15 @@
 package com.llk.emr.therapist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/therapist")
 public class TherapistController {
+
+	@PostMapping("/add")
+	public String addTherapist(){
+		return "";
+	}
 }
