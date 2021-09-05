2eaabf11ccde976259618a37eb536b0cbf6ee3c3
Varaprasad.pemmasan <Varaprasad.pemmasan@ihsmarkit.com>
2021-09-05 22:09:03 +0530

    Initial commit

---
+++ therapistservice/src/main/java/com/llk/emr/therapist/TherapistserviceApplication.java
@@ -0,0 +1,13 @@
+package com.llk.emr.therapist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherapistserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherapistserviceApplication.class, args);
+	}
+
+}
