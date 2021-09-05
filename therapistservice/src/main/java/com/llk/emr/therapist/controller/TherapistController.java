b87b9eba52f2061ab83e6f587156137f9317f4dc
Varaprasad.pemmasan <Varaprasad.pemmasan@ihsmarkit.com>
2021-09-05 22:56:49 +0530

    Controller code

--- therapistservice/src/main/java/com/llk/emr/therapist/controller/TherapistController.java
+++ therapistservice/src/main/java/com/llk/emr/therapist/controller/TherapistController.java
@@ -1,4 +1,9 @@
 package com.llk.emr.therapist.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/therapist")
 public class TherapistController {
 }
