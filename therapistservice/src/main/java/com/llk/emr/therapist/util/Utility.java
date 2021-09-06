5bc5f9513beb145b8720663adf54778516bf8583
Varaprasad.pemmasan <Varaprasad.pemmasan@ihsmarkit.com>
2021-09-06 10:32:32 +0530

    Added null check method

--- therapistservice/src/main/java/com/llk/emr/therapist/util/Utility.java
+++ therapistservice/src/main/java/com/llk/emr/therapist/util/Utility.java
@@ -1,4 +1,11 @@
 package com.llk.emr.therapist.util;
 
 public class Utility {
+	private Utility(){}
+	public static boolean isEmpty(Object obj){
+		if(obj == null){
+			return true;
+		}
+		return false;
+	}
 }
